4e2d2e6df8bb0dd9f47d09dc63f6c510bb0c1271
Ricardo Sousa <ricardosubtilsousa@gmail.com>
2022-01-11 18:55:15 +0000

    first test

---
+++ main.py
@@ -0,0 +1,10 @@
+import I2C_LCD_driver
+
+import time
+
+mylcd = I2C_LCD_driver.lcd()
+
+while True:
+    mylcd.lcd_display_string(time.strftime('%I:%M:%S %p') ,1)
+    mylcd.lcd_display_string((time.strftime('%a %b %d, 20%y')), 2)
+
